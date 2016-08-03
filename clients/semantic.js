19a54d6d661a781912e7dbfae98cfeef4e6e717c
karboom <mail@karboom.me>
2016-08-03 13:06:39 +0800

    fix: decode func

--- clients/semantic.js
+++ clients/semantic.js
@@ -1,7 +1,7 @@
 $.fn.api.ajax_error_alert = alert;
 $.fn.api.settings.onError = function(message, ele, xhr) {
     reason = xhr.getResponseHeader('X-Error-Description');
-    reason = encodeURIComponent(reason)
+    reason = decodeURIComponent(reason)
 
     $.fn.api.ajax_error_alert(reason);
     return false;
